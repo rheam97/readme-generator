7daae43dd55010aa376dc998079edd82f360edd5
Rhea <rheam97@gmail.com>
2022-01-23 03:42:44 -0500

    added switch case

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) { // look at about function in portfolio generator
-  if(!license){
-    return ''
+  switch (license){
+    case 'MIT':
+    return //svg link 
+    break;
+    case 'Apache':
+    return
+    break;
+    case 'GPL':
+    return
+    break;
   }
-  else {
-    return `
-    [${data.license}]url with svg`
-  }
-}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
